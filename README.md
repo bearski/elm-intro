2f5c9f0d75d31e9f2aae1f568955096419cc6151
bearski <santosh.sewlal@gmail.com>
2017-12-19 10:52:53 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# elm-intro
+
+[Getting Started with Elm v0.17](https://medium.com/@diamondgfx/getting-started-with-elm-11d7a53b1a78) by [Brandon Richey](https://medium.com/@diamondgfx)
+
---
+++ main.elm
@@ -0,0 +1,29 @@
+module Greeting exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+--  MAIN
+main : Html a
+main =
+  view model
+
+--  MODEL
+model : String
+model =
+  "Bearski"
+
+--  VIEW
+view : String -> Html a
+view person =
+  div []
+    [ h2 [] [ text ("Todo List For: " ++ person) ]
+    , ul []
+      [ li [] [ text "Sample Todo Item"]
+      ]
+    , input
+      [type_ "text"
+      , placeholder "New Todo Item"
+      ] []
+    , button [] [ text "Add Item" ]
+    ]
